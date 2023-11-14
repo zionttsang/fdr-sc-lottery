af9a1b361fce873699620c6c386470d3286d5d13
tsang <schwarzen221@gmail.com>
2023-11-14 18:56:15 +0800

    add file for previous version

---
+++ script/Interactions.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint64) {
+        HelperConfig helperConfig = new HelperConfig();
+        (, , address vrfCoordinatorV2, , , ) = helperConfig
+            .activeNetworkConfig();
+        return createSubscription(vrfCoordinatorV2);
+    }
+
+    function createSubscription(
+        address vrfCoordinatorV2
+    ) public returns (uint64) {
+        console.log("Creating subscription on chainId: ", block.chainid);
+        vm.startBroadcast();
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinatorV2)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("Your subscription Id is: ", subId);
+        console.log("Please update the subscriptionId in HelperConfig.s.sol");
+        return subId;
+    }
+
+    function run() external returns (uint64) {
+        return createSubscriptionUsingConfig();
+    }
+}
